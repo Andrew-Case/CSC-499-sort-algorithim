7fdacc349504f116fea5f163f7399f5740cd0105
Andrew-Case <49879085+Andrew-Case@users.noreply.github.com>
2019-10-23 17:37:21 -0400

    Add files via upload

---
+++ CSC 499 HW1/TestScript.bat
@@ -0,0 +1,10 @@
+cd C:\Users\Andrew\Andrew\Documents\CSC499HW1
+
+@py.exe C:\Users\Andrew\Andrew\Documents\CSC499HW1\sort.py %*
+
+fc /b Output SortedFile > nul
+if errorlevel 1 goto files_differ
+echo Test Passed
+
+:files_differ
+echo Test Failed
