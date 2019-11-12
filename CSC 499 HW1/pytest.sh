e0dc6339eaa59c62ee67cf54c026ddc0f8a64c75
Andrew-Case <49879085+Andrew-Case@users.noreply.github.com>
2019-11-12 18:51:54 -0500

    Add files via upload

---
+++ CSC 499 HW1/pytest.sh
@@ -0,0 +1,24 @@
+python sort.py "a"
+
+diff --brief <(SortedFile) <(outputAscdFile) >/dev/null
+comp_value=$?
+
+if [ $comp_value -eq 1 ]
+then
+    echo "test 1 passed"
+else
+    echo "test 1 failed"
+    exit 1
+
+python sort.py "d"
+
+diff --brief <(SortedFile) <(outputDescFile) >/dev/null
+comp_value=$?
+
+if [ $comp_value -eq 1 ]
+then
+    echo "test 2 passed"
+else
+    echo "test 2 failed"
+    exit 1
+fi
